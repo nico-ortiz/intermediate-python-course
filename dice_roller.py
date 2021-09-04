addaa7ef9c8939bfaea2e62b3e005d0b895cc5c7
Nicolas-Ortiz <nicortiz1234@gmail.com>
2021-09-04 00:44:03 -0300

    Tutorial complete

--- dice_roller.py
+++ dice_roller.py
@@ -2,12 +2,20 @@ import random
 
 def main():
 
+  dice_rolls = int(input('¿Cuantos dados te gustaria tirar?\n'))
+  dice_size  = int(input('¿Cuantos lados tienen los dados?\n'))
   suma = 0
-  dice_rolls = 2  
+
   for i in range(0, dice_rolls):
-    roll  = random.randint(1, 6)
-    print(f'Has lanzado un {roll}')
+    roll  = random.randint(1, dice_size)
     suma += roll
+
+    if roll == 1:
+      print(f'¡Has obtenido un {roll}! Critical Fail')
+    elif roll == dice_size:
+      print(f'¡Has obtenido un {roll}! Exito critico')
+    else:
+      print(f'Has lanzado un {roll}')
 
   print(f'Has obtenido un total de {suma}')
 
